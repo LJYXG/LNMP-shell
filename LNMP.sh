2272488b066d65a9d1916868c4481915d594fa2b
LJYXG <jianyulin2002@gmail.com>
2022-01-19 21:42:24 +0800

    just finish nginx install

--- LNMP.sh
+++ LNMP.sh
@@ -1,29 +1,73 @@
 #!/usr/bin/env bash
 
+set -e
+
 function usage
 {
     echo "usage: LNMP.sh [-n ][-m][-p ] [-a ] "
     echo ""
     echo "optional arguments:"
     echo "  -n,  --nginx                Install Nginx"
-    echo "  -m,  --mysql-server         Install Mysql-Server"
-    echo "  -p,  --php ?                Install PHP ?"
-    echo "  -a,  --all			Install All of N,M,P"
+    echo "  -m,  --mysql         Install Mysql-Server"
+    echo "  -p,  --php                Install PHP ?"
     echo "  -h,  --help			Show This"
 }
 
-case $1 in
-	-n | --nginx )	echo "23333"
-			;;
-	-h | --help )	usage
-			exit
-			;;
-	* )		usage
-			exit
-			;;
-
-esac
+function checkinstall
+{
+    if ! type $1 >/dev/null 2>&1 
+    then
+    return 0
+    else
+    return 1
+    fi
+}
 
 
+function nginx_install
+{
+    if [[ $(checkinstall nginx) == 0 ]]
+    then
+        echo "start insatlled"
+        sudo apt get install -y nginx
+        systemctl enable nginxd
+    else
+    echo "You are already installed nginx!!!"
+    fi
+    return 1
+}
 
-		 
+
+function mysql_install
+{
+    if [[ $(checkinstall mysql) == 0 ]]
+    then
+        echo "start insatlled"
+        sudo apt get install -y nginx
+        
+    else
+    echo "You are already installed nginx!!!"
+    fi
+    return 1
+}
+
+
+while [ "$1" != "" ];do
+    case $1 in
+        -n | --nginx )  shift
+                nginx_install
+                ;;
+        -m | --mysql )  shift
+                mysql_install
+                ;;
+        -h | --help )	usage
+                exit
+                ;;
+        * )		usage
+                exit
+                ;;
+
+    esac
+
+
+done
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 just easy for me reinstall LNMP
 
 maybe I'll gugugu~
+
+**注意 鸽子高估了 自己的能力 在一段时间内 鸽子就咕咕咕了 ！！！！！！！！！！**
+
+最后一次编辑： 2022年1月19日
